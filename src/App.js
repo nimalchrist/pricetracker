b4b7019eb67fd56bf3700d5df7e58e0e397ff81d
nimalchrist <chiristonimal@gmail.com>
2023-04-06 16:29:22 +0530

    nlm

--- src/App.js
+++ src/App.js
@@ -9,18 +9,18 @@ function App() {
       <div className="App">
         <nav className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
-            <Link className="navbar-brand" to={'/sign-in'}>
+            <Link className="navbar-brand" to={"/sign-in"}>
               positronX
             </Link>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-in'}>
+                  <Link className="nav-link" to={"/sign-in"}>
                     Login
                   </Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
+                  <Link className="nav-link" to={"/sign-up"}>
                     Sign up
                   </Link>
                 </li>
