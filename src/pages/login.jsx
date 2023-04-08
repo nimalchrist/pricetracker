f1576cf45958c037d1157a0662e2a9eadb23cb7b
fency <fencyj854@gmail.com>
2023-04-08 10:23:30 +0530

    change4

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,46 +1,41 @@
-import React, { Component } from 'react'
-export default class Login extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Sign In</h3>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
+import React, { useState } from "react";
+import "./login.css";
+
+export default function App() {
+  const [state, setState] = useState(true);
+
+  const RegisterForm = (
+    <form id="Regform">
+      <input type="text" placeholder="username" />
+      <input type="E-mail" placeholder="Email" />
+      <input type="password" placeholder="password" />
+      <button type="submit" className="btn">
+        Register
+      </button>
+    </form>
+  );
+
+  const LoginForm = (
+    <form id="loginform">
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+      <button type="submit" className="btn">
+        Login
+      </button>
+      <a href="#pass">Forgot password</a>
+    </form>
+  );
+
+  return (
+    <div className="acc-page">
+      <div className="form-container">
+        <div className="form-btn">
+          <span onClick={() => setState(false)}>Login</span>
+          <span onClick={() => setState(true)}>Register</span>
+          <hr id="indicator" />
+          {state ? RegisterForm : LoginForm}
         </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
-            <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
-            />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Forgot <a href="#pass">password?</a>
-        </p>
-      </form>
-    )
-  }
+      </div>
+    </div>
+  );
 }
